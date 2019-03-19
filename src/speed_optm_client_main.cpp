c32e8e3655beb2d7aec441e5ce4efe5cf1be591d
Alessandro Palleschi <42964650+alessandropalleschi@users.noreply.github.com>
2019-03-19 23:22:26 +0100

    Update speed_optm_client_main.cpp

--- src/speed_optm_client_main.cpp
+++ src/speed_optm_client_main.cpp
@@ -1,6 +1,5 @@
 #include "speed_optimization_pkg/speed_optimization_client.h"
 
-
 int main(int argc, char **argv)
 {
     ros::init(argc, argv, "Path Optimization Client");
@@ -16,16 +15,10 @@ int main(int argc, char **argv)
       ROS_ERROR("Something went wrong");
       return 1;
     }  
-    ros::Rate f(100);
-
-while (ros::ok())
-{
-  f.sleep(); 
-  ros::spinOnce();
-  client.send_trajectory(client.srv_.response.optm_traj);
-  f.sleep(); 
-  break;
-} 
+    
+    ros::Duration(0.1).sleep();
+    client.send_trajectory(client.srv_.response.optm_traj);
+    ros::spin();
 
   return 0;
 }
